1d211c927e6d8f3fb5a213c2c15676a23760b6e1
Quinn <74791898+griffique@users.noreply.github.com>
2021-12-19 17:36:24 -0500

    final?

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 // import Sound from "react-sound";
 import { motion } from "framer-motion";
 import styles from "./styles.module.sass";
+import pickles from "./images/pickles.jpg";
+import toki from "./images/toki.jpg";
+import griffins from "./images/griffins.jpg";
 
 const cardVariants = {
   flipped: {
@@ -25,8 +28,10 @@ const Card = () => {
     setFading(!fading);
   };
 
-  const handleClick = () => {
-    setFlipped(!flipped);
+  const handleClick = (location) => {
+    if (location === "card" && flipped === false) {
+      setFlipped(!flipped);
+    }
   };
 
   return (
@@ -35,7 +40,7 @@ const Card = () => {
         <div>
           <motion.div
             className={styles.card}
-            onClick={() => handleClick()}
+            onClick={() => handleClick("card")}
             variants={cardVariants}
             animate={flipped ? "flipped" : "notFlipped"}
           >
@@ -44,25 +49,72 @@ const Card = () => {
                 flipped ? `${styles.backContent}` : `${styles.frontContent}`
               }
               onMouseDown={() => {
-                fadeFunction();
+                if (!flipped) {
+                  fadeFunction();
+                }
               }}
               onMouseUp={() => {
-                setTimeout(() => {
-                  fadeFunction();
-                }, 500);
+                if (!flipped) {
+                  setTimeout(() => {
+                    fadeFunction();
+                  }, 500);
+                }
               }}
               animate={{ opacity: fading ? 0 : 1 }}
             >
-              <div className={fading || flipped ? "is-hidden" : ""}>
-                {" "}
-                CARD IMAGE{" "}
+              <div
+                className={fading || flipped ? "is-hidden" : styles.imageArea}
+              >
+                <div className={styles.topImage}>
+                  <img src={griffins} alt="Quinn and Hayes"></img>
+                </div>
+                <div className={styles.bottomImages}>
+                  <img src={pickles} alt="Pickles the cat."></img>
+                  <img src={toki} alt="Toki the cat."></img>
+                </div>
               </div>
               <div
-                className={
-                  fading || !flipped ? "is-hidden" : `${styles.message}`
-                }
+                className={fading || !flipped ? "is-hidden" : styles.message}
               >
-                MESSAGE
+                <h1>Happy Holidays from the Griffins</h1>
+                <p>
+                  In 2021 the Griffins moved back to Columbus, OH! We are very
+                  excited to be closer to our family and friends.
+                </p>
+                <p>
+                  Hayes has been hard at work on international online music
+                  school{" "}
+                  <a target="_blank" href="https://www.mandolinsecrets.com/">
+                    Mandolin Secrets
+                  </a>{" "}
+                  and recently launched the Guitar Club section of the site.
+                </p>
+                <p>
+                  Quinn completed the{" "}
+                  <a
+                    target="_blank"
+                    href="https://www.shecodes.io/stories/Quinn-junior-react-developer"
+                  >
+                    SheCodes workshop
+                  </a>{" "}
+                  in fall 2021 and transitioned into a new career in web
+                  development with{" "}
+                  <a target="_blank" href="https://s4netquest.com/">
+                    S4 Netquest.
+                  </a>
+                </p>
+                <p>
+                  Pickles and Toki are well and enjoying having more space in
+                  the new house. We wish you all the best for 2022!
+                </p>
+                <button
+                  className="button is-success mt-1"
+                  onClick={() => {
+                    setFlipped(false);
+                  }}
+                >
+                  BACK
+                </button>
               </div>
             </motion.div>
           </motion.div>
